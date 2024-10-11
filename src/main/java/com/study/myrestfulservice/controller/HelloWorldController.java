1d7a541242aa2e2a817bf0fb87e5abe9c2e2c69b
seulGit <seulgi9421@gmail.com>
2024-10-11 18:51:29 +0900

    241011 - path variable 이용 - 가변 URI사용

--- src/main/java/com/study/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/study/myrestfulservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.study.myrestfulservice.controller;
 
 import com.study.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloworldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloworldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
