07dc36722af8cc1a70ad1bd52cf821f91478115a
seulGit <seulgi9421@gmail.com>
2024-10-13 21:11:52 +0900

    241013 - 사용자 목록 조회를 위한 API 구현 - HTTP GET/POST method

---
+++ src/main/java/com/study/myrestfulservice/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.study.myrestfulservice.controller;
+
+import com.study.myrestfulservice.bean.User;
+import com.study.myrestfulservice.dao.UserDaoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+    }
+}
