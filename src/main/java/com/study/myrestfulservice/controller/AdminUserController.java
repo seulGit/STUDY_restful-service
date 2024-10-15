b116e3fc070695766af2d1c340bd931bbd90ccf6
seulGit <seulgi9421@gmail.com>
2024-10-16 00:10:25 +0900

    241015 - 프로그래밍으로 제어하는 Filtering - 개별 사용자 조회

---
+++ src/main/java/com/study/myrestfulservice/controller/AdminUserController.java
@@ -0,0 +1,47 @@
+package com.study.myrestfulservice.controller;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.study.myrestfulservice.bean.AdminUser;
+import com.study.myrestfulservice.bean.User;
+import com.study.myrestfulservice.dao.UserDaoService;
+import com.study.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+    private UserDaoService service;
+
+    public AdminUserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    // /admin/users/{id}
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        AdminUser adminUser = new AdminUser();
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        } else {
+            BeanUtils.copyProperties(user, adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+}
