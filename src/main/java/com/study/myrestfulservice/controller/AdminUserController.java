22cc5dc9e76b161e02ea39a3af8a60057cd137f1
seulGit <seulgi9421@gmail.com>
2024-10-16 00:29:25 +0900

    241015 - URI를 이용한 버전관리

--- src/main/java/com/study/myrestfulservice/controller/AdminUserController.java
+++ src/main/java/com/study/myrestfulservice/controller/AdminUserController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.study.myrestfulservice.bean.AdminUser;
+import com.study.myrestfulservice.bean.AdminUserV2;
 import com.study.myrestfulservice.bean.User;
 import com.study.myrestfulservice.dao.UserDaoService;
 import com.study.myrestfulservice.exception.UserNotFoundException;
@@ -26,9 +27,9 @@ public class AdminUserController {
         this.service = service;
     }
 
-    // /admin/users/{id}
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
+    // /admin/v1/users/{id}
+    @GetMapping("/v1/users/{id}")
+    public MappingJacksonValue retrieveUser4AdminV1(@PathVariable int id) {
         User user = service.findOne(id);
 
         AdminUser adminUser = new AdminUser();
@@ -41,6 +42,29 @@ public class AdminUserController {
 
         SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    // /admin/v2/users/{id}
+    @GetMapping("/v2/users/{id}")
+    public MappingJacksonValue retrieveUser4AdminV2(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        AdminUserV2 adminUser = new AdminUserV2();
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        } else {
+            BeanUtils.copyProperties(user, adminUser);
+            adminUser.setGrade("VIP"); // grade
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "grade");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
         mapping.setFilters(filters);
