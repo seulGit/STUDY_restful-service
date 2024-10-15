485bd68a2831f44224737cb9df66ecd75706549b
seulGit <seulgi9421@gmail.com>
2024-10-16 00:17:36 +0900

    241015 - 프로그래밍으로 제어하는 Filtering - 전체  사용자 조회

--- src/main/java/com/study/myrestfulservice/controller/AdminUserController.java
+++ src/main/java/com/study/myrestfulservice/controller/AdminUserController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin")
 public class AdminUserController {
@@ -40,6 +43,29 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsers4Admin() {
+        List<User> users = service.findAll();
+
+        List<AdminUser> adminUsers = new ArrayList<>();
+        AdminUser adminUser = null;
+
+        for (User user : users) {
+            adminUser = new AdminUser();
+            BeanUtils.copyProperties(user, adminUser);
+
+            adminUsers.add(adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUsers);
         mapping.setFilters(filters);
 
         return mapping;
