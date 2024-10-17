645d0de0bbd2a3d71d8e903114d8dc3157e5c5e0
seulGit <seulgi9421@gmail.com>
2024-10-17 16:03:24 +0900

    241017 - Spring Data JPA를 이요한 초기 데이터 생성

--- src/main/java/com/study/myrestfulservice/bean/User.java
+++ src/main/java/com/study/myrestfulservice/bean/User.java
@@ -2,6 +2,10 @@ package com.study.myrestfulservice.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,8 +17,12 @@ import java.util.Date;
 @AllArgsConstructor
 @JsonIgnoreProperties(value = {"password", "ssn"})
 @Schema(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
+@Table(name = "users")
 public class User {
     @Schema(title = "사용자 ID", description = "사용자 ID는 자동 생성됩니다.")
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Schema(title = "사용자 이름", description = "사용자 이름을 입력합니다.")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values (90001, now(), 'User1', 'test111', '701010-1111111');
+insert into users(id, join_date, name, password, ssn) values (90002, now(), 'User2', 'test222', '801212-1111111');
+insert into users(id, join_date, name, password, ssn) values (90003, now(), 'User3', 'test333', '901110-1111111');
