604c4e1bcb3e98c9bd2953b14421973e4cc44eb5
seulGit <seulgi9421@gmail.com>
2024-10-17 18:15:04 +0900

    241017 - to create new post using JPA - HTTP POST Method

--- src/main/java/com/study/myrestfulservice/controller/UserJpaController.java
+++ src/main/java/com/study/myrestfulservice/controller/UserJpaController.java
@@ -3,6 +3,7 @@ package com.study.myrestfulservice.controller;
 import com.study.myrestfulservice.bean.Post;
 import com.study.myrestfulservice.bean.User;
 import com.study.myrestfulservice.exception.UserNotFoundException;
+import com.study.myrestfulservice.repository.PostRepository;
 import com.study.myrestfulservice.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
@@ -24,9 +25,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class UserJpaController {
 
     private UserRepository userRepository;
+    private PostRepository postRepository;
 
-    public UserJpaController(UserRepository userRepository) {
+    public UserJpaController(UserRepository userRepository,
+                             PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     // /jpa/users
@@ -92,5 +96,26 @@ public class UserJpaController {
         }
 
         return user.get().getPosts();
+    }
+
+    @PostMapping("/users/{id}/posts")
+    public ResponseEntity createPost(@PathVariable int id, @RequestBody Post post) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        User user = userOptional.get();
+
+        post.setUser(user);
+
+        postRepository.save(post);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(post.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
---
+++ src/main/java/com/study/myrestfulservice/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.study.myrestfulservice.repository;
+
+import com.study.myrestfulservice.bean.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
